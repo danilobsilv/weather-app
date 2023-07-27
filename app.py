b40fe379a9e94ed0924900f5af74d00f9ab29d9f
Danilo Bruno <danilobsilv@gmail.com>
2023-07-26 20:50:16 -0400

    tested new imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from backend.weather.weather_info import ww
+from backend.weather.weather_info import WeatherInfo
 from backend.location.get_user_location import get_user_location
 
 if __name__ == "__main__":
