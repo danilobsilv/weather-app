f85f8310468eb5461a2f0518efc4eabfb630e2ae
Danilo Bruno <danilobsilv@gmail.com>
2023-07-27 23:17:53 -0400

    get: user's location lat/long

--- backend/location/get_user_location.py
+++ backend/location/get_user_location.py
@@ -1,11 +1,5 @@
 import geocoder
-from weather.weather_api_key import my_key
-from weather.weather_info import WeatherInfo
 
 def get_user_location():
     g = geocoder.ip('me')
-    return g.latlng  # Retorna uma tupla com latitude e longitude
-
-
-# latitude, longitude = obter_localizacao_usuario()
-# print(f"Latitude: {latitude}, Longitude: {longitude}")
+    return g.latlng
