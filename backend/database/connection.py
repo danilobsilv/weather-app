3a5524b60ab83f7edf01bf29e3a0857effcc3fe4
Danilo Bruno <danilobsilv@gmail.com>
2023-08-23 23:05:38 -0400

    fix: database connection

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,27 +1,14 @@
-from db_keys import database_keys
+import sqlite3
+import datetime
 
+connection = sqlite3.connect("weather_app_db.db")
 
-import mysql.connector
-
-
-def database_connect():
-      connection = None
-      try:
-            connection = mysql.connector.connect(
-                  host = database_keys.get("host"),
-                  user = database_keys.get("user"),
-                  password = database_keys.get("password"),
-                  database = database_keys.get("database")
-            )
-            if connection.is_connected():
-                  print("Conectado ao banco de dados com sucesso!")
-
-      except mysql.connector.Error as db_conn_error:
-            print(f"Erro ao conectar-se ao banco de dados --> {db_conn_error}")
-
-      finally:
-            if connection and connection.is_connected():
-                  connection.close()
-                  print("Conexão ao MySQL fechada!")
-
-database_connect()
+connection.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        user_email TEXT NOT NULL,
+        user_name TEXT NOT NULL,
+        user_password TEXT NOT NULL,
+        user_creation DATETIME NOT NULL
+    )
+""")
