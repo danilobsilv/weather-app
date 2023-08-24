253c1d8157af3e76fdc540fa95e998fa8e804f32
Danilo Bruno da Silva <96261259+danilobsilv@users.noreply.github.com>
2023-08-23 23:10:23 -0400

    fix: database conn

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -4,7 +4,8 @@ import datetime
 connection = sqlite3.connect("weather_app_db.db")
 
 connection.execute("""
-    CREATE TABLE IF NOT EXISTS users (
+        CREATE TABLE IF NOT EXISTS users (
+        
         user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         user_email TEXT NOT NULL,
         user_name TEXT NOT NULL,
