ca4602f2a19e90863d9f85138ff3140cb0c02455
Danilo Bruno <danilobsilv@gmail.com>
2023-09-03 19:40:06 -0400

    tests

--- app.py
+++ app.py
@@ -1,39 +1,30 @@
 import sys
-
-from backend.exceptions.userNotFound import UserNotFound
-
-from backend.weather.weatherInfo import WeatherInfo
-from backend.weather.weatherApiKey import api_key
-
-from backend.location.getUserLocation import get_user_location
+import time
+import json
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from frontend.views.loginView import Ui_MainWindow
 
+# from frontend.views.createAccountView import Ui_CreateAccount
+# from frontend.views.loginView import Ui_MainWindow
+# from frontend.ViewsController import ViewsController
 
+from backend.exceptions.exceptions import ErrorException
+from backend.weather.weatherInfo import WeatherInfo
+from backend.weather.weatherApiKey import api_key
+from backend.location.getUserLocation import get_user_location
+from backend.weather.weatherInfo import WeatherInfo
 from backend.models.userModel import UserModel
 
 if __name__ == "__main__":
-      # lati, longi = get_user_location()
- 
-      # ww = WeatherInfo()
-      
-      # print(ww.get_temperature(api_key.get("key"), lati, longi))
-      
+    
+      # app = QApplication(sys.argv)
+      # main_window = QMainWindow()
+      # ui_main = Ui_MainWindow()
+      # ui_create_account = Ui_CreateAccount()
 
-      app = QApplication(sys.argv)
-      mainWindow = QMainWindow()
-      ui = Ui_MainWindow()
-      ui.setupUi(mainWindow)
-      mainWindow.show()
-      sys.exit(app.exec_())
-                   
-      # pass
+      # ui_main.setupUi(main_window)
+      # views_controller = ViewsController(ui_main, ui_create_account)
 
-      # from backend.controllers.user_controller import UserController 
-      # user = UserController()
-      # print(user.checkUserEmail("danilobsilv@gmail.com"))
-
-
-      # cont = UserModel()
-      # (cont.get_user_by_id("sksks"))                                                                                                                      
+      # main_window.show()
+      # sys.exit(app.exec_())                                                                                 
+      pass
